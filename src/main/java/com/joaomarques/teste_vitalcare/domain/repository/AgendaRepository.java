1d052804652546a934af8a82173fc55f2a61df46
Joao Marques <joao.pereira92@fatec.sp.gov.br>
2024-11-23 14:15:12 -0300

    chore: corrige versao do sistema

--- src/main/java/com/joaomarques/teste_vitalcare/domain/repository/AgendaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.joaomarques.teste_vitalcare.domain.repository;
-
-import com.joaomarques.teste_vitalcare.domain.entity.AgendaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AgendaRepository extends JpaRepository<AgendaEntity, Long> {}
--- src/main/java/com/joaomarques/vitalcare/domain/repository/AgendaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.joaomarques.vitalcare.domain.repository;
-
-//@Repository
-public interface AgendaRepository {//extends JpaRepository<AgendaEntity, Long> {
-//    Optional<AgendaEntity> findByUsuarioEntityId(Long idUsuario);
-}
